ee5fd557a55197f7befd944633b8440ad5c40785
Jose Ortiz <a20182162@pucp.edu.pe>
2021-11-23 19:31:43 -0500

    Initial commit

---
+++ src/main/java/pucp/edu/pe/lab9/HelloApplication.java
@@ -0,0 +1,9 @@
+package pucp.edu.pe.lab9;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
